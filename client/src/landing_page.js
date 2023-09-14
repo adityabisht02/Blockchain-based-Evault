a7800f83641577296a49735fc9c8429525c6d212
Pradyumn Garg <86672147+Pradyumn-Garg@users.noreply.github.com>
2023-09-14 18:39:47 +0530

    landing page ini commit

--- client/src/landing_page.js
+++ client/src/landing_page.js
@@ -15,6 +15,7 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
+import HouseIcon from '@mui/icons-material/House';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import Tooltip from '@mui/material/Tooltip';
@@ -23,12 +24,38 @@ import AdbIcon from '@mui/icons-material/Adb';
 import { Link } from "react-router-dom";
 import { render } from '@testing-library/react';
 import { useNavigate } from 'react-router-dom';
+import Ministryname from './Images/Ministryname.png';
+import banner1 from './Images/banner1.png';
+import banner2 from './Images/banner2.png';
+import zIndex from '@mui/material/styles/zIndex';
 
 
 function Landing_page() {
+  const navigate = useNavigate();
   return (
     <div >
-        
+      <AppBar position="static" className='dummy5' elevation={0} style={{ background: '#e4e3db', height: "45px" }}>
+        <Container className='cont' maxWidth="xl">
+          <Toolbar disableGutters>
+            <img src={Ministryname} alt="My logo" width="85" height="33" style={{ marginTop: "-15px", marginLeft: "15px" }} sx={{ display: { xs: 'none', md: 'flex' }, mr: 1, width: '10vw', hright: '10vh' }} onClick={(e) => { navigate('/') }} />
+            <Typography component="div" sx={{ flexGrow: 1, marginTop: "-14px", color: "black", fontSize: "11px" }}>
+            <b>GOVERNMENT OF INDIA</b>
+            </Typography>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <img src={banner2} alt="Banner" width="33%" />
+      <img src={banner1} alt="Banner" width="14.5%" style={{ marginLeft: "705px"}}/>
+      <AppBar position="static" className='dummy5' style={{ background: '#0c0d0d', height: "45px" }}>
+        <Container className='cont' maxWidth="xl">
+          <Toolbar disableGutters>
+          <div style={{ display: 'flex' }}>
+            <HouseIcon sx={{marginTop: "-20px"}}/>
+          </div>
+          </Toolbar>
+        </Container>
+      </AppBar>
+
     </div>
   );
 }
