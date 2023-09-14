dbd3b79812793969d123906d64975dcd72e00871
Pradyumn Garg <86672147+Pradyumn-Garg@users.noreply.github.com>
2023-09-14 16:15:30 +0530

    frontend init

---
+++ client/src/landing_page.js
@@ -0,0 +1,36 @@
+import React, { useState, useRef, useCallback, useEffect, useContext } from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link2 from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+import { Link } from "react-router-dom";
+import { render } from '@testing-library/react';
+import { useNavigate } from 'react-router-dom';
+
+
+function Landing_page() {
+  return (
+    <div >
+        
+    </div>
+  );
+}
+
+export default Landing_page;
