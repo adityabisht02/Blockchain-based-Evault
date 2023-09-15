8bfe5e330f37c9fc5c66fa11f36fc6e397cd22fc
MonikaRaghav <105781189+MonikaRaghav@users.noreply.github.com>
2023-09-15 07:18:00 +0530

    About us page

---
+++ client/src/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+  <div>  
+    <img src = "SupremeCourt.jpg" alt="Card Image"></img>
+    <h1>Cases 1</h1>
+    <p>This case is about the forgery</p>
+</div>
+  )
+}
+
+export default Card
